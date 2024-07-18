06b61924c88f3b6eec46c228e0fa10cfa8a4791e
Sravan <sravan.karuturi@strivr.com>
2024-07-17 19:44:57 -0700

    Initial Commit

---
+++ MetVApp/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  MetVApp
+//
+//  Created by Sravan Karuturi on 7/17/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ MetVAppTests/MetVAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetVAppTests.swift
+//  MetVAppTests
+//
+//  Created by Sravan Karuturi on 7/17/24.
+//
+
+import Testing
+@testable import MetVApp
+
+struct MetVAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
